c8e33bc6509d28bc4fcce7690fd8e17b1ffd78ef
Elias Prevost <e.prevost@ensi.lan>
2024-11-19 15:30:35 +0100

    TresholdCamera_Good_DestroyTreshold_ToFix

---
+++ Assets/PreProdElias/Scripts/DestroyTiles.cs
@@ -0,0 +1,27 @@
+using System;
+using Proto.Script;
+using UnityEngine;
+
+namespace PreProdElias.Scripts
+{
+    public class DestroyTiles : MonoBehaviour
+    {
+        private CubeSpawner _cubeSpawner;
+
+        private void Awake()
+        {
+            _cubeSpawner = FindObjectOfType<CubeSpawner>();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (_cubeSpawner.CubeCount !=0)
+            {
+                _cubeSpawner.CubeCount -= 1;
+            }
+            _cubeSpawner.Cubes.Remove(other.GetComponent<Cube>());
+            Destroy(other.gameObject);
+            _cubeSpawner.SpawnCube = true;
+        }
+    }
+}
